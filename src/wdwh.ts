21c5010821849c09be40623b2ec04120fe3cf56e
Jakub <jakubhanula0@gmail.com>
2025-12-08 13:00:01 +0100

    1.7.12 fix dev mode (2)

--- src/wdwh.ts
+++ src/wdwh.ts
@@ -3,6 +3,5 @@
 import { dev } from "./dev"
 import { build } from "./build"
 
-if (process.argv.includes(`dev`)) dev()
-else if (process.argv.includes(`build`)) await build()
-else console.log(`wrong command: "${process.argv.at(2)}"\ntry "dev" | "build"`)
+if (process.argv.includes(`build`)) await build()
+else dev()
